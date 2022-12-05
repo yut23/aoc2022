a5cc51048d488b5caf36fa8e8a36e3cdaf3288f5
yut23 <yut23@gvljohnsons.com>
2022-12-05 12:58:58 -0500

    Day 4 part 1 complete

---
+++ src/day4.cpp
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * File:        day4.cpp
+ *
+ * Author:      yut23
+ * Created:     2022-12-05
+ *****************************************************************************/
+
+#include "lib.h"
+#include <algorithm>
+#include <sstream>
+#include <string>
+
+struct Assignment {
+  int start{-1};
+  int end{-1};
+};
+
+std::istream &operator>>(std::istream &is, Assignment &a) {
+  is >> a.start >> a.end;
+  return is;
+}
+
+struct Pair {
+  Assignment first{};
+  Assignment second{};
+
+  bool includes() {
+    return (first.start <= second.start && first.end >= second.end) ||
+           (second.start <= first.start && second.end >= first.end);
+  }
+};
+
+std::istream &operator>>(std::istream &is, Pair &p) {
+  is >> p.first >> p.second;
+  return is;
+}
+
+int main(int argc, char **argv) {
+  auto infile = parse_args(argc, argv);
+
+  // read file line-by-line
+  std::string line;
+  int count = 0;
+  while (std::getline(infile, line)) {
+    // simple and stupid: replace '-' and ',' with space
+    std::ranges::replace_if(
+        line, [](char c) { return c == '-' || c == ','; }, ' ');
+    std::stringstream ss{line};
+    // do stuff
+    Pair pair{};
+    ss >> pair;
+
+    if (pair.includes()) {
+      ++count;
+    }
+  }
+  std::cout << count << std::endl;
+  return EXIT_SUCCESS;
+}
