535f8f8102b77fd3821ef57cc58c9e7f04998c28
yut23 <yut23@gvljohnsons.com>
2022-12-08 20:26:26 -0500

    Clean up day 8 (too much STL)

--- src/day8.cpp
+++ src/day8.cpp
@@ -8,12 +8,9 @@
 #include "lib.h"
 #include <algorithm>  // for count_if
 #include <cassert>    // for assert
-#include <concepts>   // for same_as
 #include <cstdint>    // for uint8_t
 #include <functional> // for plus
 #include <iostream>   // for cout
-#include <iterator>   // for input_iterator, sentinel_for, iter_value_t
-#include <memory>     // for shared_ptr, make_shared, pointer_traits
 #include <numeric>    // for transform_reduce
 #include <string>     // for string, getline
 #include <vector>     // for vector
@@ -21,78 +18,73 @@
 namespace aoc::day8 {
 
 struct Tree {
-    // pack the height and a visible flag into one byte (if the compiler
-    // cooperates) 4 bits can hold from 0 to 15; we only need 0 to 9
+    // pack the height and a visible flag into one byte
+    // 4 bits can hold 0 to 15; we only need 0 to 9
     std::uint8_t height : 4;
     bool visible : 1 {false};
 
     explicit Tree(std::uint8_t height_) : height(height_) {}
 };
-static_assert(sizeof(Tree) == 1, "tree struct not packed");
-
-template <std::input_iterator I, std::sentinel_for<I> S>
-requires std::same_as<
-    typename std::pointer_traits<std::iter_value_t<I>>::element_type, Tree>
-void mark_visible(I first, S last) {
-    std::uint8_t tallest = (*first)->height;
-    for (auto it = first; it != last; ++it) {
-        // the first tree is always visible
-        if (it == first || (*it)->height > tallest) {
-            (*it)->visible = true;
-            tallest = (*it)->height;
-        }
-    }
-}
 
 class Forest {
   private:
-    std::vector<std::vector<std::shared_ptr<Tree>>> trees_row_major{};
-    std::vector<std::vector<std::shared_ptr<Tree>>> trees_col_major{};
+    std::vector<std::vector<Tree>> trees{};
+    int side_length = 0;
 
   public:
+    int size() const { return side_length; }
     void add_row(const std::string &line);
+    // Update the visibility of the tree at (r, c) given a tree of height
+    // `tallest` in front of it. May also update `tallest`.
+    void update_visibility(int r, int c, int &tallest);
     // mark the trees that are visible from any direction
-    void mark();
+    void mark_visible();
     int count_visible() const;
 };
 
 void Forest::add_row(const std::string &line) {
-    if (trees_row_major.empty()) {
-        assert(trees_col_major.empty());
-        // initialize column major view
-        trees_col_major.resize(line.length());
-    }
-    assert(trees_col_major.size() == line.length());
     // add a new row
-    trees_row_major.emplace_back();
-    auto col_it = trees_col_major.begin();
-    for (auto char_it = line.cbegin(); char_it != line.cend();
-         ++char_it, ++col_it) {
-        auto tree = std::make_shared<Tree>(*char_it - '0');
+    trees.emplace_back();
+    ++side_length;
+    for (char c : line) {
         // add the tree to the last row
-        trees_row_major.back().push_back(tree);
-        // add the tree to the end of the corresponding column
-        col_it->push_back(tree);
+        trees.back().emplace_back(c - '0');
     }
 }
 
-void Forest::mark() {
-    for (const auto &row : trees_row_major) {
-        mark_visible(row.begin(), row.end());
-        mark_visible(row.rbegin(), row.rend());
+void Forest::update_visibility(int row, int col, int &tallest) {
+    auto &tree = trees[row][col];
+    if (tree.height > tallest) {
+        tree.visible = true;
+        tallest = tree.height;
     }
-    for (const auto &col : trees_col_major) {
-        mark_visible(col.begin(), col.end());
-        mark_visible(col.rbegin(), col.rend());
+}
+
+void Forest::mark_visible() {
+    assert(size() > 0);
+    assert(size() == trees.size() && size() == trees[0].size());
+    for (int i = 0; i < size(); ++i) {
+        int tallest_row = -1, tallest_col = -1;
+        for (int j = 0; j < size(); ++j) {
+            update_visibility(i, j, tallest_row);
+            update_visibility(j, i, tallest_col);
+        }
+    }
+    // same thing, but in reverse
+    for (int i = size() - 1; i >= 0; --i) {
+        int tallest_row = -1, tallest_col = -1;
+        for (int j = size() - 1; j >= 0; --j) {
+            update_visibility(i, j, tallest_row);
+            update_visibility(j, i, tallest_col);
+        }
     }
 }
 
 int Forest::count_visible() const {
     return std::transform_reduce(
-        trees_row_major.cbegin(), trees_row_major.cend(), 0, std::plus{},
-        [](const auto &row) {
+        trees.cbegin(), trees.cend(), 0, std::plus{}, [](const auto &row) {
             return std::ranges::count_if(
-                row, [](const auto &tree) { return tree->visible; });
+                row, [](const auto &tree) { return tree.visible; });
         });
 }
 
@@ -110,7 +102,7 @@ int main(int argc, char **argv) {
         // construct tree
         forest.add_row(line);
     }
-    forest.mark();
+    forest.mark_visible();
     std::cout << forest.count_visible() << std::endl;
     return 0;
 }
