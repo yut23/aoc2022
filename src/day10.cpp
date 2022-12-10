9cce5fb175c14333e1f60b81746ecd046e8ab4c3
yut23 <yut23@gvljohnsons.com>
2022-12-10 16:31:51 -0500

    Day 10 part 2 complete

--- src/day10.cpp
+++ src/day10.cpp
@@ -7,20 +7,23 @@
 
 #include "lib.h"
 #include <cassert>  // for assert
+#include <cstdlib>  // for abs
 #include <iostream> // for cout
 #include <string>   // for string, getline
+#include <vector>   // for vector
 
 int main(int argc, char **argv) {
     std::ifstream infile = aoc::parse_args(argc, argv);
 
     // read file line-by-line
     std::string command;
+    std::vector<std::string> image{};
     int X = 1;
     int next_X = X;
     int arg;
     int wait = 0;
     int sum = 0;
-    for (int cycle = 1; cycle <= 220; ++cycle, --wait) {
+    for (int cycle = 1; cycle <= 240; ++cycle, --wait) {
         if (wait == 0) {
             X = next_X;
             if (!(infile >> command)) {
@@ -45,7 +48,17 @@ int main(int argc, char **argv) {
                           << sum << std::endl;
             }
         }
+        int beam_pos = (cycle - 1) % 40;
+        if (beam_pos == 0) {
+            image.emplace_back(40, ' ');
+        }
+        if (std::abs(X - beam_pos) <= 1) {
+            image.back()[beam_pos] = '#';
+        }
     }
     std::cout << sum << std::endl;
+    for (const auto &line : image) {
+        std::cout << line << std::endl;
+    }
     return 0;
 }
