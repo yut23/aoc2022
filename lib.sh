a37df8b27567bb523350752cc17326674108dff5
yut23 <yut23@gvljohnsons.com>
2022-12-18 21:11:58 -0500

    Allow workon to make binaries in parallel

--- lib.sh
+++ lib.sh
@@ -2,9 +2,24 @@
 
 make_quiet() {
   # make a target only if make thinks it's necessary (outputs nothing if not)
-  if [[ $# -gt 1 ]]; then
+  targets=()
+  args=()
+  while [[ $# -gt 0 ]]; do
+    args+=("$1")
+    case $1 in
+      -*)
+        # allow any number of options
+        ;;
+      *)
+        targets+=("$1")
+        ;;
+    esac
+    shift
+  done
+  set -- "${args[@]}"
+  if [[ ${#targets[@]} -gt 1 ]]; then
     >&2 echo "Internal error: make_quiet should only be passed one target"
     return 1
   fi
-  make -q "$1" 2>/dev/null || make "$1"
+  make -q "${targets[@]}" 2>/dev/null || make "$@"
 }
