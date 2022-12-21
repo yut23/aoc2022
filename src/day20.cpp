5eca866d97264196d80860e1ae1a56799afb4827
yut23 <yut23@gvljohnsons.com>
2022-12-21 13:03:13 -0500

    Day 20 part 2 complete

--- src/day20.cpp
+++ src/day20.cpp
@@ -7,9 +7,8 @@
 
 #include "lib.h"
 #include <cstdlib>  // for abs
-#include <iostream> // for cout
+#include <iostream> // for cout, cerr
 #include <memory>   // for unique_ptr, make_unique
-#include <string>   // for string, getline
 #include <utility>  // for move
 #include <vector>   // for vector
 
@@ -52,18 +51,123 @@ void CircularLinkedNode<T>::unlink() {
 }
 
 template <typename T>
-void print_list(std::ostream &os, CircularLinkedNode<T> *head) {
-    auto *ptr = head;
-    while (true) {
+void advance(CircularLinkedNode<T> *&ptr, long count = 1) {
+    for (long i = 0; i < std::abs(count); ++i) {
+        if (count > 0) {
+            ptr = ptr->next;
+        } else {
+            ptr = ptr->prev;
+        }
+    }
+}
+
+template <typename T>
+class CircularLinkedList {
+  public:
+    using Node = CircularLinkedNode<T>;
+
+  private:
+    using UniquePtr = std::unique_ptr<Node>;
+    std::vector<UniquePtr> nodes;
+    Node *head = nullptr;
+    Node *zero = nullptr;
+
+  public:
+    long size() const { return nodes.size(); }
+    void push_back(const T &value);
+    void mix();
+    T calc_sum() const;
+
+    template <typename U>
+    friend std::ostream &operator<<(std::ostream &,
+                                    const CircularLinkedList<U> &);
+};
+
+template <typename T>
+void CircularLinkedList<T>::push_back(const T &value) {
+    std::unique_ptr<Node> node_ptr = std::make_unique<Node>(value);
+    Node *raw_ptr = node_ptr.get();
+    if (size() == 0) {
+        head = raw_ptr;
+    } else {
+        nodes.back()->insert_after(raw_ptr);
+    }
+    if (value == 0) {
+        zero = raw_ptr;
+    }
+    nodes.emplace_back(std::move(node_ptr));
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const CircularLinkedList<T> &list) {
+    auto *ptr = list.head;
+    for (int i = 0; i < list.size(); ++i) {
         os << ptr->data;
         ptr = ptr->next;
-        if (ptr == head) {
-            break;
-        } else {
+        if (ptr != list.head) {
             os << ", ";
         }
     }
-    os << "\n";
+    return os;
+}
+
+template <typename T>
+void CircularLinkedList<T>::mix() {
+    if constexpr (aoc::DEBUG) {
+        std::cerr << "initial arrangement:\n";
+    }
+    for (const std::unique_ptr<Node> &node : nodes) {
+        if constexpr (aoc::DEBUG) {
+            std::cerr << *this << "\n";
+            std::cerr << "\nmoving " << node->data << ":\n";
+        }
+        if (node->data == 0) {
+            continue;
+        }
+        auto *ptr = node.get();
+        if (ptr == head) {
+            head = ptr->next;
+            if constexpr (aoc::DEBUG) {
+                std::cerr << "(updated head to " << head->data
+                          << " after unlinking)\n";
+            }
+        }
+        node->unlink();
+        int shift = node->data % (size() - 1);
+        if constexpr (aoc::DEBUG) {
+            std::cerr << "advancing by " << shift << "\n";
+        }
+        advance(ptr, shift);
+        if (ptr->next == head && node->data > 0) {
+            head = node.get();
+            if constexpr (aoc::DEBUG) {
+                std::cerr << "(updated head to " << head->data
+                          << " after inserting)\n";
+            }
+        }
+        if (shift > 0) {
+            ptr->insert_after(node.get());
+        } else {
+            ptr->insert_before(node.get());
+        }
+    }
+    if constexpr (aoc::DEBUG) {
+        std::cerr << *this << "\n";
+    }
+}
+
+template <typename T>
+T CircularLinkedList<T>::calc_sum() const {
+    T sum = 0;
+    for (int i = 1; i <= 3; ++i) {
+        Node *ptr = zero;
+        advance(ptr, (i * 1000) % size());
+        if constexpr (aoc::DEBUG) {
+            std::cerr << i * 1000 << "th number: " << ptr->data << "\n";
+        }
+        sum += ptr->data;
+    }
+    return sum;
 }
 
 } // namespace aoc::day20
@@ -73,69 +177,25 @@ int main(int argc, char **argv) {
 
     using namespace aoc::day20;
     // need to store pointers here since the vector may change capacity
-    std::vector<std::unique_ptr<CircularLinkedNode<int>>> nodes;
+    CircularLinkedList<long> list_1;
+    CircularLinkedList<long> list_2;
     // read file
-    int value;
-    CircularLinkedNode<int> *zero;
+    long value;
     while (infile >> value) {
-        auto node_ptr = std::make_unique<CircularLinkedNode<int>>(value);
-        if (nodes.size() > 0) {
-            nodes.back()->insert_after(node_ptr.get());
-        }
-        if (value == 0) {
-            zero = node_ptr.get();
-        }
-        nodes.emplace_back(std::move(node_ptr));
+        list_1.push_back(value);
+        list_2.push_back(value * 811589153);
     }
 
-    CircularLinkedNode<int> *head = nodes.front().get();
-    if constexpr (aoc::DEBUG) {
-        std::cerr << "initial arangement:\n";
-    }
-    for (const auto &node : nodes) {
+    list_1.mix();
+    std::cout << list_1.calc_sum() << "\n";
+
+    for (int i = 0; i < 10; ++i) {
+        list_2.mix();
         if constexpr (aoc::DEBUG) {
-            print_list(std::cerr, head);
-            std::cerr << "\nmoving " << node->data << ":\n";
-        }
-        if (node->data == 0) {
-            continue;
-        }
-        auto *ptr = node.get();
-        if (ptr == head) {
-            head = ptr->next;
-        }
-        node->unlink();
-        for (int i = 0; i < std::abs(node->data); ++i) {
-            if (node->data > 0) {
-                ptr = ptr->next;
-            } else {
-                ptr = ptr->prev;
-            }
-        }
-        if (ptr->next == head) {
-            if constexpr (aoc::DEBUG) {
-                std::cerr << "(updated head)\n";
-            }
-            head = node.get();
-        }
-        if (node->data > 0) {
-            ptr->insert_after(node.get());
-        } else {
-            ptr->insert_before(node.get());
+            std::cerr << "\n";
         }
     }
-    if constexpr (aoc::DEBUG) {
-        print_list(std::cerr, head);
-    }
+    std::cout << list_2.calc_sum() << "\n";
 
-    int sum = 0;
-    for (int i = 1; i <= 3; ++i) {
-        auto *ptr = zero;
-        for (int index = (i * 1000) % nodes.size(); index > 0; --index) {
-            ptr = ptr->next;
-        }
-        sum += ptr->data;
-    }
-    std::cout << sum << "\n";
     return 0;
 }
