23719c15cee61617a6987958a2372c5589f4b85a
yut23 <yut23@gvljohnsons.com>
2023-01-02 23:41:10 -0500

    Day 17 part 2 fully working

--- src/day17.cpp
+++ src/day17.cpp
@@ -6,57 +6,79 @@
  *****************************************************************************/
 
 #include "lib.h"
-#include <algorithm> // for transform, for_each, max
-#include <array>     // for array
-#include <iostream>  // for cout, cerr
-#include <iterator>  // for back_inserter, distance
-#include <map>       // for map
-#include <string>    // for string
-#include <tuple>     // for tuple, make_tuple
-#include <vector>    // for vector
+#include <algorithm>   // for transform, for_each, max
+#include <array>       // for array
+#include <bit>         // for bit_width
+#include <iostream>    // for cout, cerr
+#include <iterator>    // for back_inserter, distance
+#include <map>         // for map
+#include <string>      // for string
+#include <type_traits> // for remove_const_t
+#include <vector>      // for vector
 
 namespace aoc::day17 {
 
-// uses 9 bits in total, with the LSB and MSB used for wall collision detection
-using line_t = unsigned short;
+// uses 7 bits for each line
+using line_t = unsigned char;
 using rock_shape_t = std::array<line_t, 4>;
 
 // these shapes have the lowest part at the front (only affects the L shape)
 const std::array<rock_shape_t, 5> ROCK_SHAPES{{
     {
-        0b0'0011110'0,
-        0b0'0000000'0,
-        0b0'0000000'0,
-        0b0'0000000'0,
+        0b0011110,
+        0b0000000,
+        0b0000000,
+        0b0000000,
     },
     {
-        0b0'0001000'0,
-        0b0'0011100'0,
-        0b0'0001000'0,
-        0b0'0000000'0,
+        0b0001000,
+        0b0011100,
+        0b0001000,
+        0b0000000,
     },
     {
-        0b0'0011100'0,
-        0b0'0000100'0,
-        0b0'0000100'0,
-        0b0'0000000'0,
+        0b0011100,
+        0b0000100,
+        0b0000100,
+        0b0000000,
     },
     {
-        0b0'0010000'0,
-        0b0'0010000'0,
-        0b0'0010000'0,
-        0b0'0010000'0,
+        0b0010000,
+        0b0010000,
+        0b0010000,
+        0b0010000,
     },
     {
-        0b0'0011000'0,
-        0b0'0011000'0,
-        0b0'0000000'0,
-        0b0'0000000'0,
+        0b0011000,
+        0b0011000,
+        0b0000000,
+        0b0000000,
     },
 }};
 constexpr std::array<int, 5> ROCK_HEIGHTS{{1, 3, 3, 4, 2}};
 
-constexpr line_t WALL_MASK = 0b1'0000000'1;
+struct DropPositions {
+    unsigned short raw;
+
+    DropPositions() : raw(0) {
+        // fill with invalid positions
+        for (int i = 0; i < 5; ++i) {
+            raw |= 7 << (i * 3);
+        }
+    }
+
+    unsigned char get(int rock_index) {
+        return static_cast<unsigned char>(raw >> (rock_index * 3)) & 0b111;
+    }
+    void set(int rock_index, unsigned char pos) {
+        unsigned short mask = 0b111 << (rock_index * 3);
+        raw = (raw & ~mask) | ((pos & 0b111) << (rock_index * 3));
+    }
+    void copy_from(const DropPositions &other, int rock_index) {
+        unsigned short mask = 0b111 << (rock_index * 3);
+        raw = (raw & ~mask) | (other.raw & mask);
+    }
+};
 
 class Board {
     std::vector<line_t> lines{};
@@ -75,10 +97,13 @@ class Board {
 
     struct LoopInfo {
         long rock_number;
-        long floor_height;
+        long height;
     };
 
-    std::map<std::tuple<int, int, unsigned long>, LoopInfo> loop_cache{};
+    static constexpr unsigned int cache_size = 2;
+    std::map<const std::array<unsigned short, cache_size>, LoopInfo>
+        loop_cache{};
+    std::array<DropPositions, cache_size> drop_pos_arr{};
     bool found_loop = false;
     bool at_loop_start = false;
     LoopInfo loop_start;
@@ -91,16 +116,13 @@ class Board {
     bool move_rock_down();
     void push_rock(bool debug);
 
-    // discard all lines below the given iterator
-    void remove_full_line(std::vector<line_t>::iterator);
-
     void save_to_cache();
     void drop_rock();
 
   public:
     explicit Board(const std::string &);
 
-    long height() const;
+    long height() const { return floor_height + internal_height; }
     void drop_until(long count);
 
     friend std::ostream &operator<<(std::ostream &, const Board &);
@@ -113,41 +135,9 @@ Board::Board(const std::string &jets) {
     save_to_cache();
 }
 
-void Board::save_to_cache() {
-    if (internal_height > 4) {
-        return;
-    }
-    unsigned long packed_lines = 0;
-    for (int i = 0; i < internal_height; ++i) {
-        unsigned char rock =
-            static_cast<unsigned char>((lines[i] & ~WALL_MASK) >> 1);
-        packed_lines |= rock << (i * 8);
-    }
-    auto key = std::make_tuple(rock_index, jet_index, packed_lines);
-    if (loop_cache.contains(key)) {
-        found_loop = true;
-        loop_start = loop_cache[key];
-        loop_end = LoopInfo{rock_number, floor_height};
-        loop_cache.clear();
-        if constexpr (aoc::DEBUG) {
-            std::cerr << "found loop starting at rock number "
-                      << loop_start.rock_number << " and floor height "
-                      << loop_start.floor_height << ", with length "
-                      << loop_end.rock_number - loop_start.rock_number
-                      << " and height "
-                      << loop_end.floor_height - loop_start.floor_height
-                      << "\n";
-        }
-    } else {
-        loop_cache[key] = LoopInfo{rock_number, floor_height};
-    }
-}
-
-inline long Board::height() const { return internal_height + floor_height; }
-
 void Board::ensure_enough_lines(long new_height) {
     if (static_cast<long>(lines.size()) < new_height) {
-        lines.resize(new_height, WALL_MASK);
+        lines.resize(new_height, 0);
     }
 }
 
@@ -163,8 +153,14 @@ bool Board::will_collide(const Direction &dir) const {
     for (auto rock_it = rock_shape.cbegin(); rock_it != rock_shape.cend();
          ++rock_it, ++line_it) {
         if (dir == Direction::left) {
+            if (*rock_it & (1 << 6)) {
+                return true;
+            }
             rock_val = *rock_it << 1;
         } else if (dir == Direction::right) {
+            if (*rock_it & (1 << 0)) {
+                return true;
+            }
             rock_val = *rock_it >> 1;
         } else {
             rock_val = *rock_it;
@@ -211,17 +207,27 @@ bool Board::move_rock_down() {
     return true;
 }
 
-void Board::remove_full_line(std::vector<line_t>::iterator full_it) {
-    ++full_it;
-    if constexpr (aoc::DEBUG) {
-        std::cerr << "full line found at "
-                  << floor_height + std::distance(lines.begin(), full_it) - 1
-                  << ":\n"
-                  << *this << "\n";
+void Board::save_to_cache() {
+    std::remove_const_t<typename decltype(loop_cache)::key_type> test_key{};
+    std::ranges::transform(drop_pos_arr, test_key.begin(),
+                           [](const auto &drop_pos) { return drop_pos.raw; });
+    auto it = loop_cache.find(test_key);
+    if (it != loop_cache.end()) {
+        found_loop = true;
+        loop_start = it->second;
+        loop_end = LoopInfo{rock_number, height()};
+        loop_cache.clear();
+        if constexpr (aoc::DEBUG) {
+            std::cerr << "found loop starting at rock number "
+                      << loop_start.rock_number << " and floor height "
+                      << loop_start.height << ", with length "
+                      << loop_end.rock_number - loop_start.rock_number
+                      << " and height " << loop_end.height - loop_start.height
+                      << "\n";
+        }
+    } else {
+        loop_cache[test_key] = LoopInfo{rock_number, height()};
     }
-    floor_height += std::distance(lines.begin(), full_it);
-    internal_height -= std::distance(lines.begin(), full_it);
-    lines.erase(lines.begin(), full_it);
 }
 
 void Board::drop_rock() {
@@ -230,7 +236,6 @@ void Board::drop_rock() {
     rock_pos = internal_height + 3;
     rock_shape = ROCK_SHAPES[rock_index];
     rock_height = ROCK_HEIGHTS[rock_index];
-    rock_index = (rock_index + 1) % ROCK_SHAPES.size();
     ensure_enough_lines(rock_pos + rock_shape.size());
 
     if constexpr (aoc::DEBUG) {
@@ -240,7 +245,7 @@ void Board::drop_rock() {
         }
     }
 
-    // main tick loop
+    // main update loop
     while (true) {
         // push with jet
         push_rock(debug);
@@ -259,20 +264,19 @@ void Board::drop_rock() {
         }
     }
 
+    unsigned char drop_pos = 0;
     internal_height = std::max(internal_height, rock_pos + rock_height);
     // add rock to stationary lines
     auto line_it = lines.begin() + rock_pos;
-    // keep track of any full lines so we can forget about anything below them
-    auto full_it = lines.end();
     for (auto rock_it = std::begin(rock_shape); rock_it != std::end(rock_shape);
          ++rock_it, ++line_it) {
         *line_it |= *rock_it;
-
-        // check for full lines
-        if (*line_it == 0b1'1111111'1) {
-            full_it = line_it;
-        }
+        drop_pos = std::max(drop_pos, std::bit_width(*rock_it));
     }
+    for (unsigned int i = 0; i < drop_pos_arr.size() - 1; ++i) {
+        drop_pos_arr[i].copy_from(drop_pos_arr[i + 1], rock_index);
+    }
+    drop_pos_arr.back().set(rock_index, drop_pos);
     rock_pos = 0;
     rock_height = 0;
 
@@ -282,18 +286,15 @@ void Board::drop_rock() {
         }
     }
 
-    if (full_it != lines.end()) {
-        // only keep lines above the the full one
-        remove_full_line(full_it);
-        if (!found_loop) {
-            save_to_cache();
-        }
-    }
     if (found_loop) {
         at_loop_start = (rock_number - loop_start.rock_number) %
                             (loop_end.rock_number - loop_start.rock_number) ==
                         0;
+    } else if (rock_index == 0) {
+        save_to_cache();
     }
+
+    rock_index = (rock_index + 1) % ROCK_SHAPES.size();
 }
 
 void Board::drop_until(long count) {
@@ -307,11 +308,10 @@ void Board::drop_until(long count) {
         std::cerr << "skipping forward...\n";
     }
     long loop_size = loop_end.rock_number - loop_start.rock_number;
-    long loop_height = loop_end.floor_height - loop_start.floor_height;
-    while (rock_number + loop_size < count) {
-        rock_number += loop_size;
-        floor_height += loop_height;
-    }
+    long loop_height = loop_end.height - loop_start.height;
+    long iter_count = (count - rock_number) / loop_size;
+    rock_number += loop_size * iter_count;
+    floor_height += loop_height * iter_count;
     if constexpr (aoc::DEBUG) {
         std::cerr << "skipped to rock " << rock_number
                   << ", height=" << height() << "\n";
@@ -330,7 +330,7 @@ std::ostream &operator<<(std::ostream &os, const Board &board) {
         // assert(i < static_cast<long>(board.lines.size()));
         long rock_idx = i - board.rock_pos;
         os << '|';
-        for (line_t mask = 1 << 7; mask != 1; mask >>= 1) {
+        for (line_t mask = 1 << 6; mask != 0; mask >>= 1) {
             if (rock_idx >= 0 && rock_idx < board.rock_height &&
                 board.rock_shape[rock_idx] & mask) {
                 os << '@';
